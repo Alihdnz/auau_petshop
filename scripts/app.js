37b32dd00245ce96a5c569eb2845a72fac0e9247
Alisson Diniz <alisson.diniz@jet.intra>
2022-07-15 18:39:17 -0300

    ajustes no front

--- scripts/app.js
+++ scripts/app.js
@@ -24,29 +24,65 @@
 // } 
 
 
-$(document).ready(function() {
-    $('.responsive').lightSlider({
-        item:4,
-        loop:false,
-        slideMove:2,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:600,
-        responsive : [
-            {
-                breakpoint:800,
-                settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
-            },
-            {
-                breakpoint:480,
-                settings: {
-                    item:2,
-                    slideMove:1
-                  }
-            }
-        ]
-    });  
-  });
+
+
+
+
+
+function closeCart() {
+
+    
+    
+
+    $("#minicart").addClass("hidden")
+
+
+}
+
+// $("#minicartBtn").click(function() {
+//         console.log('44')
+// }
+//     )
+
+// Open Minicart
+
+function openCart() {
+     $("#minicart").removeClass("hidden")
+
+    //  $("#minicart").addClass(".visible")
+
+    
+}
+
+// Slider
+
+// $(document).ready(function() {
+//     $('.responsive').lightSlider({
+//         item:4,
+//         loop:false,
+//         slideMove:2,
+//         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+//         speed:600,
+//         responsive : [
+//             {
+//                 breakpoint:800,
+//                 settings: {
+//                     item:1,
+//                     slideMove:1,
+//                     slideMargin:6,
+//                   }
+//             },
+//             {
+//                 breakpoint:400,
+//                 settings: {
+//                     item:1,
+//                     slideMove:1
+//                   }
+//             }
+//         ]
+//     });  
+//   });
+
+
+
+  
